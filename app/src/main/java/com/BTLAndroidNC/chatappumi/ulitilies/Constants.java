30416728dfe1c4bf35dd1c61a3311a161675706b
VanAnh290903 <nguyenvananh29092003@gmail.com>
2024-05-28 01:17:11 +0700

    init application

---
+++ app/src/main/java/com/BTLAndroidNC/chatappumi/ulitilies/Constants.java
@@ -0,0 +1,7 @@
+package com.BTLAndroidNC.chatappumi.ulitilies;
+
+public class Constants {
+    // Đặt các hằng số sử sụng trong app
+    // VD: public static final String KEY_USER = "User";
+    public static final String KEY_PREFERENCE_ACCOUNT = "account";
+}
